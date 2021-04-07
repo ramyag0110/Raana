a612a13a3487ed32c89878f19006fe3a40ec7801
ramyag0110 <ramya.gajula@syf.com>
2021-04-07 12:19:44 +0000

    save

---
+++ Conditional example.js
@@ -0,0 +1,58 @@
+
+// find the given number is odd or even
+n = 11
+console.log(n%2);
+
+console.log(n%2==0);
+
+if(n%2==1){ //if block
+    console.log("odd number");
+}
+else{ //else block
+    console.log("even number");
+}
+
+//find the greater number x or y
+let x=10;
+   let  y=20;
+if(x>y){
+    console.log("x is greater");
+
+}else if (y>x){
+    console.log("y is greater");
+
+
+
+
+
+// + : adition
+// - : sub
+// * : mul
+// / : div
+// % : mod
+// = : assign
+// == : compare data
+// === : compare data and type
+// < : less than
+// > : greater than
+
+
+
+//find capital of state
+switch (statecode){
+    case "ts":
+    console.log("hyderabad");
+    break;
+    case "tn":
+ console.log ("Chennai");
+    break;
+    case "ka":
+    console.log("bangalore");
+    break;
+    default:
+        console.log("statecode not found");
+        break:
+
+
+}
+
--- Demo.js
+++ Demo.js
@@ -1,1 +1,18 @@
+"use strict"//force user to declare variable
 
+
+console.log("hello");
+
+//assign to value to x
+
+let x=10
+let y=20
+let z=x+y;
+
+console.log(z);
+
+x=100
+y=300
+z=x+y;
+
+console.log(z);
