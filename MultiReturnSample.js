43e6b59539a2f7b32a3eca5c2b142addf6be6a80
ramyag0110 <ramya.gajula@syf.com>
2021-04-07 19:57:51 +0000

    Day 3

---
+++ MultiReturnSample.js
@@ -0,0 +1,17 @@
+  
+//write a function to return odd numbers between a range
+
+function getOddNumbersRange(rStart, rEnd) {
+    
+    let oddNumbers=[];
+    for(let r = rStart;r<=rEnd;r++){
+        if (r%2==1) {
+            oddNumbers.push(r);
+            // console.log(r);            
+        }
+    }
+    return oddNumbers;
+}
+
+let res = getOddNumbersRange(10,50);
+console.log(res);
